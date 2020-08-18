77ac77e81b734326362100590f9e8a9d3ebd1691
kishlaychhajer <kishlay.chhajer@alumni.ubc.ca>
2020-08-18 13:25:25 +0530

    Initial Commit

---
+++ SwiftChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftChat
+//
+//  Created by Kishlay Chhajer on 2020-08-18.
+//  Copyright © 2020 Kishlay Chhajer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
